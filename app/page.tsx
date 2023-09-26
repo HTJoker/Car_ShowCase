404a02f6f610e5182523304a4012b00db2e03dd2
Hersh Thakur <hersh.thakur@gmail.com>
2023-09-26 08:47:47 +0100

    added assets to the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Hello World</div>;
+  return <main className="overflow-hidden">Hello World</main>;
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 
-
 const Navbar = () => {
   return <div>Navbar</div>;
 };
